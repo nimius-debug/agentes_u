088cf3282957ccdba730e7fae458a0f10562ec8a
Alquimista <jorgegil9706@gmail.com>
2024-08-27 12:13:39 -0400

    the main app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,8 +3,10 @@ import streamlit as st
 from st_discord_nav import st_discord_nav
 from page_components.GmapExtractor import g_map_extractor
 from page_components.Logout import logout
+st.set_page_config(page_title="Leads", page_icon="🔍", layout="centered", initial_sidebar_state="collapsed")
+
 def main():
-    
+   
     # Main Streamlit app starts here
     st.markdown(f"""
     <style>
